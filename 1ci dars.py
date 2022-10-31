466dc5f4470c369d233515787ef78aabaa8c41d8
shakh06 <abdullayevmuhammad0037@gmail.com>
2022-10-31 16:56:32 +0500

    Initial commit

---
+++ 1ci dars.py
@@ -0,0 +1,85 @@
+# a= 'xurram soirov'
+# print(a)
+# print(a.capitalize())
+
+
+# age = int(input('Yoshingizni kiriting:'))
+# print(2022-age)
+
+
+# son1 = 22
+# son2 = 42
+# son3 = son1 - son2
+# print(son3)
+
+
+# a = 32
+# b = 12
+# natija = a * b 
+# print(natija / 10)
+
+
+# bir = 54
+# ikki = 72
+# javob = bir / ikki
+# print(javob * 12 +12)
+
+
+# son1 = 23
+# son2 = 2
+# print(son1 ** son2)
+
+
+# a = 41
+# b = 76
+# print(a < b)
+# print(a > b)
+
+
+# a = 'salom'
+# b = True
+# c = 2
+# d = 2.1
+# e = 5.4
+# print(d - e)
+
+
+# a = 12
+# b = 12
+# print(a == b)
+# print(a != b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2 ci dars.py
@@ -0,0 +1,125 @@
+import math
+# 1
+# a = math.pi
+# print(a)
+
+# 2
+# a = 3
+# b = 13
+# c = a * b
+# print(c)
+# print(math.sqrt(c))
+
+# 3
+# print(math.ceil(12.3))
+# print(math.floor(12.3))
+# print(math.sin(33))
+# print(math.factorial(6))
+# print(math.log(125, 5))
+# print(math.cos(120))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
